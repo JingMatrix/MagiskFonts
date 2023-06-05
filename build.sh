76024ab5f66a68908310c68b4bcc773d008947c4
JingMatrix <jingmatrix@gmail.com>
2023-06-05 12:31:32 +0200

    Avoid re-building on local machine

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #! bash
 
+[[ -e MagiskFonts.zip ]] && rm MagiskFonts.zip
+if [[ -z $TERMUX_VERSION ]]; then
+	echo "Skip building font-scan if not in termux"
+	7z a MagiskFonts.zip META-INF customize.sh module.prop system tool
+	exit
+fi
+pushd fontconfig
 pushd fontconfig
 rm -rf build
 meson build
