6ab20abe616f440d63cafac43fc19763ea8c0ea3
JingMatrix <jingmatrix@gmail.com>
2023-06-05 13:24:12 +0200

    Update fontconfig

--- build.sh
+++ build.sh
@@ -6,13 +6,12 @@ if [[ -z $TERMUX_VERSION ]]; then
 	7z a MagiskFonts.zip META-INF customize.sh module.prop system tool
 	exit
 fi
-pushd fontconfig
+
 pushd fontconfig
 rm -rf build
 meson build
 ninja -C build
-cp build/fc-scan/fc-scan build/src/libfontconfig.so* ../tool
-echo '<fontconfig></fontconfig>' > tool/fonts.conf
+cp build/fc-scan/fc-scan build/src/libfontconfig.so* build/subprojects/freetype2/libfreetype.so* ../tool
 popd
 rm -rf system/etc
 rm -rf MagiskFonts.zip
